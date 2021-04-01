37cc0b053051719b79b1f01304ac7c31267f3820
Clive <clive.raines@gmail.com>
2021-04-01 19:20:02 +0100

    Exercise 1 - first push

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 
 from todo_app.flask_config import Config
+from todo_app.data import session_items
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -8,7 +11,18 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = session_items.get_items()
+    return render_template("index.html", items = items)
+
+@app.route('/newitem', methods=['POST'])
+def CreateNew():
+
+    #call add item
+    data = request.form.get('newitem')
+    session_items.add_item(data)
+    #re render
+    items = session_items.get_items()
+    return render_template("index.html", items = items)
 
 
 if __name__ == '__main__':
