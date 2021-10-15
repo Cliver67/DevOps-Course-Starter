da49638e8ff13119f18e2a5a36a2a552cd0d8a67
Clive <clive.raines@gmail.com>
2021-10-15 06:43:08 +0100

    end day

--- todo_app/tests/test_flask.py
+++ todo_app/tests/test_flask.py
@@ -2,6 +2,9 @@ import dotenv, pytest
 from todo_app import app
 from unittest.mock import patch
 
+#methods to get test board and list_id's
+
+
 
 @pytest.fixture
 def client():
@@ -21,14 +24,23 @@ def test_index_page(client):
 @patch('requests.get')
 def test_index_page(mock_get_requests, client):
     # Replace call to requests.get(url) with our own function
-    mock_get_requests.side_effect = mock_get_lists
+    mock_get_requests.side_effect = mock_get_cards
     response = client.get('/')
 
-def mock_get_lists(url, params):
-    if url == f'https://api.trello.com/1/boards/{TEST_BOARD_ID}/lists':
-        response = Mock()
-        # sample_trello_lists_response should point to some test response data
-        response.json.return_value = sample_trello_lists_response
-        return response
+#def mock_get_lists(url, params):
+#    if url == f'https://api.trello.com/1/boards/{TEST_BOARD_ID}/lists':
+#        response = Mock()
+#        # sample_trello_lists_response should point to some test response data
+#        response.json.return_value = sample_trello_lists_response
+#        return response
+#   return None
 
+def mock_get_cards(url,params):
+    #url to match  -  'https://api.trello.com/1/boards/' + get_boardid) +'/cards?fields=name,idList,dateLastActivity'
+    if url == f'https://api.trello.com/1/boards/{TEST_BOARD_ID}/cards?fields=name,idlist,dateLastActivity':
+        response = Mock()
+        #sample_trello_cards response with the requested fields
+        response.json.return_value = sample_trello_cards_response
+        return response
     return None
+
