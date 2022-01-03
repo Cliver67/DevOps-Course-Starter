24450cf387ff5a5e5f348833443cf93913b7a227
Clive <clive.raines@gmail.com>
2022-01-03 19:27:45 +0000

    need help

--- todo_app/tests/test_flask.py
+++ todo_app/tests/test_flask.py
@@ -1,15 +1,18 @@
-import dotenv, pytest
-from todo_app import app
-from unittest.mock import patch , MagicMock
 
+from todo_app import app
+from unittest import mock
+import dotenv, pytest
+from unittest.mock import patch 
+import json
 #methods to get test board and list_id's
 
 
 
 @pytest.fixture
 def client():
-    #use our test integration config instead f the "real" version
-    file_path = dotenv.find_dotenv('.env_test')
+    #use our test integration config instead of the "real" version
+    file_path = dotenv.find_dotenv('.env.test')
+    
 
     #create the new app
     test_app = app.create_app()
@@ -24,23 +27,23 @@ def test_index_page(client):
 @patch('requests.get')
 def test_index_page(mock_get_requests, client):
     # Replace call to requests.get(url) with our own function
-    mock_get_requests.side_effect = mock_get_cards
+    mock_get_requests.side_effect = mock_get_cards('https://api.trello.com/1/boards/001/cards?fields=name,idList,dateLastActivity',1)
     response = client.get('/')
 
-#def mock_get_lists(url, params):
-#    if url == f'https://api.trello.com/1/boards/{TEST_BOARD_ID}/lists':
-#        response = Mock()
-#        # sample_trello_lists_response should point to some test response data
-#        response.json.return_value = sample_trello_lists_response
-#        return response
-#   return None
+
 
 def mock_get_cards(url,params):
-    #url to match  -  'https://api.trello.com/1/boards/' + get_boardid) +'/cards?fields=name,idList,dateLastActivity'
-    if url == f'https://api.trello.com/1/boards/{TEST_BOARD_ID}/cards?fields=name,idlist,dateLastActivity':
-        response = Mock()
+    #url to match  -  'https://api.trello.com/1/boards/' + get_boardid() +'/cards?fields=name,idList,dateLastActivity'
+    #if url == f'https://api.trello.com/1/boards/{TEST_BOARD_ID}/cards?fields=name,idList,dateLastActivity':
+        response = mock.Mock()
         #sample_trello_cards response with the requested fields
         response.json.return_value = sample_trello_cards_response
         return response
-    return None
+    #return None
 
+def sample_trello_cards_response():
+    x = '{"id":"id001","name":"hello i am a card","idList":"003"}'
+    y = json.loads(x)
+    print(y["name"])
+
+    return (y)
